3ab12b259a562b75253eff7548cb86abd8dc8d24
Laís Barreto Vieira <laisbarreto.tech@gmail.com>
2023-12-06 09:49:37 -0300

    update app.js

--- app.js
+++ app.js
@@ -4,45 +4,49 @@ const { IssuePost } = require("./src/classes/IssuePost/IssuePost");
 const { Comment } = require("./src/classes/Comment/Comment");
 const { ImageUploader } = require("./src/classes/ImageUploader/ImageUploader");
 
-try {
-  const user1 = new User("John Doe", "john@example.com", "+551234567890");
-  const user2 = new User("Jane Smith", "jane@example.com");
+(async () => {
+  try {
+    const user1 = new User("John Doe", "john@example.com", "+551234567890");
+    const user2 = new User("Jane Smith", "jane@example.com");
 
-  const post1 = Post.createPost({
-    user: user1,
-    textPost:
-      "Problema na Rua X - Estrada precária, precisa de reparos urgentes.",
-  });
+    const post1 = Post.createPost({
+      user: user1,
+      textPost:
+        "Problema na Rua X - Estrada precária, precisa de reparos urgentes.",
+    });
 
-  const imageUploader1 = new ImageUploader(post1.postId, {
-    name: "imagem.jpg",
-    type: "image/jpeg",
-  });
-  imageUploader1.handleImageSelect();
+    const imageUploader1 = new ImageUploader(post1.postId, {
+      name: "imagem.jpg",
+      type: "image/jpeg",
+    });
+    imageUploader1.handleImageSelect();
 
-  const issuePost1 = new IssuePost({
-    user: user2,
-    textPost: "Alagamento na Praça Y",
-    issue: "Alagamento",
-    location: "Praça Y",
-  });
+    const issuePost1 = new IssuePost({
+      user: user2,
+      textPost: "Alagamento na Praça Y",
+      issue: "Alagamento",
+      location: "Praça Y",
+    });
 
-  const imageUploader2 = new ImageUploader(issuePost1.postId, {
-    name: "imagem_alagamento.jpg",
-    type: "image/jpeg",
-  });
-  imageUploader2.handleImageSelect();
+    const imageUploader2 = new ImageUploader(issuePost1.postId, {
+      name: "imagem_alagamento.jpg",
+      type: "image/jpeg",
+    });
+    imageUploader2.handleImageSelect();
 
-  const comment1 = new Comment({
-    user: user1,
-    textPost: "Espero que resolvam logo!",
-    parentPost: issuePost1,
-  });
+    const comment1 = new Comment({
+      user: user1,
+      textPost: "Espero que resolvam logo!",
+      parentPost: issuePost1,
+    });
 
-  comment1.addComment("Jane Doe", "Concordo, é uma situação séria.");
+    comment1.addComment("Jane Doe", "Concordo, é uma situação séria.");
 
-  const allPosts = Post.getAllPosts();
-  console.log("Todas as postagens:", allPosts);
-} catch (error) {
-  console.error("Erro ao criar postagem:", error.message);
-}
+    const allPosts = Post.getAllPosts();
+    console.log("Todas as postagens:", allPosts);
+  } catch (error) {
+    console.error("Erro ao criar postagem:", error.message);
+  } finally {
+    process.exit(0);
+  }
+})();
