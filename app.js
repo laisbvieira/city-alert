f074109e9be5000c9932586f7aff9e083dc312bd
Laís Barreto Vieira <laisbarreto.tech@gmail.com>
2023-12-06 15:19:02 -0300

    update deploy test

--- app.js
+++ app.js
@@ -1,56 +1,60 @@
-// const { User } = require("./src/classes/User/User");
-// const { Post } = require("./src/classes/Post/Post");
-// const { IssuePost } = require("./src/classes/IssuePost/IssuePost");
-// const { Comment } = require("./src/classes/Comment/Comment");
-// const { ImageUploader } = require("./src/classes/ImageUploader/ImageUploader");
+const { User } = require("./src/classes/User/User");
+const { Post } = require("./src/classes/Post/Post");
+const { IssuePost } = require("./src/classes/IssuePost/IssuePost");
+const { Comment } = require("./src/classes/Comment/Comment");
+const { ImageUploader } = require("./src/classes/ImageUploader/ImageUploader");
+const express = require("express");
 
-// (async () => {
-//   try {
-//     const user1 = new User("John Doe", "john@example.com", "+551234567890");
-//     const user2 = new User("Jane Smith", "jane@example.com");
+const app = express();
 
-//     const post1 = Post.createPost({
-//       user: user1,
-//       textPost:
-//         "Problema na Rua X - Estrada precária, precisa de reparos urgentes.",
-//     });
+(async () => {
+  try {
+    const user1 = new User("John Doe", "john@example.com", "+551234567890");
+    const user2 = new User("Jane Smith", "jane@example.com");
 
-//     const imageUploader1 = new ImageUploader(post1.postId, {
-//       name: "imagem.jpg",
-//       type: "image/jpeg",
-//     });
-//     imageUploader1.handleImageSelect();
+    const post1 = Post.createPost({
+      user: user1,
+      textPost:
+        "Problema na Rua X - Estrada precária, precisa de reparos urgentes.",
+    });
 
-//     const issuePost1 = new IssuePost({
-//       user: user2,
-//       textPost: "Alagamento na Praça Y",
-//       issue: "Alagamento",
-//       location: "Praça Y",
-//     });
+    const imageUploader1 = new ImageUploader(post1.postId, {
+      name: "imagem.jpg",
+      type: "image/jpeg",
+    });
+    imageUploader1.handleImageSelect();
 
-//     const imageUploader2 = new ImageUploader(issuePost1.postId, {
-//       name: "imagem_alagamento.jpg",
-//       type: "image/jpeg",
-//     });
-//     imageUploader2.handleImageSelect();
+    const issuePost1 = new IssuePost({
+      user: user2,
+      textPost: "Alagamento na Praça Y",
+      issue: "Alagamento",
+      location: "Praça Y",
+    });
 
-//     const comment1 = new Comment({
-//       user: user1,
-//       textPost: "Espero que resolvam logo!",
-//       parentPost: issuePost1,
-//     });
+    const imageUploader2 = new ImageUploader(issuePost1.postId, {
+      name: "imagem_alagamento.jpg",
+      type: "image/jpeg",
+    });
+    imageUploader2.handleImageSelect();
 
-//     comment1.addComment("Jane Doe", "Concordo, é uma situação séria.");
+    const comment1 = new Comment({
+      user: user1,
+      textPost: "Espero que resolvam logo!",
+      parentPost: issuePost1,
+    });
 
-//     const allPosts = Post.getAllPosts();
-//     console.log("Todas as postagens:", allPosts);
-//   } catch (error) {
-//     console.error("Erro ao criar postagem:", error.message);
-//   } finally {
-//     process.on("SIGTERM", () => {
-//       console.log("Recebido sinal SIGTERM. Encerrando o processo Node.js");
-//     });
-//     process.exit();
-//   }
-// })();
-console.log("ok");
+    comment1.addComment("Jane Doe", "Concordo, é uma situação séria.");
+
+    const allPosts = Post.getAllPosts();
+    console.log("Todas as postagens:", allPosts);
+  } catch (error) {
+    console.error("Erro ao criar postagem:", error.message);
+  } finally {
+    console.log("Fim.");
+  }
+})();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+app.listen(3000);
